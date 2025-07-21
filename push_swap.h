a94bcbfc331021fb7a631bb4607a666f0d42f46b
Jon <JonNovoa@users.noreply.github.com~>
2025-07-21 15:45:53 +0200

    inicio

---
+++ push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jon <jon@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 12:34:04 by jon               #+#    #+#             */
+/*   Updated: 2025/07/21 14:31:29 by jon              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*next;
+}	t_node;
+
+void	free_stack(void);
+void	exit_error(void);
+int		is_sorted(t_node *stack);
+
+#endif
